cf180f9cdc04ea03f441d3f3a2a0f8ee6a9386dd
Jayesh Savkare <jayesh.savkare.work@gmail.com>
2023-05-03 14:17:35 +0530

    Added 11 List Methods.py

---
+++ 11 List Methods.py
@@ -0,0 +1,34 @@
+numbers = [1,2,3,4,5]
+
+
+# Add a new element at end of our list
+# numbers.append(6)
+# print(numbers)
+# Output = [1, 2, 3, 4, 5, 6]
+
+
+# Insert Method - Used to add a Value in List.
+# Values that this Method Expects are = (index of first element:integer , object:any type) ~ Object is which we have to add//
+# numbers.insert(0,-1)
+# print(numbers)
+# Output = [-1, 1, 2, 3, 4, 5, 6]
+
+
+# Clear Method - Used to remove all Values.
+# Does not Expects any Value.
+# numbers.clear()
+# print(numbers)
+# Output = []
+
+
+# In Operator
+# To Check given value exists in our List.
+print(1 in numbers) #Boolean Expression - It checks 1 exists in our List.
+# Output = True
+
+
+# len Function
+# Used to count elements in given list.
+print(len(numbers))
+# Output = 5
+
