e1135d631b82ef6a3c14dadd17386b65c530a011
Jayesh Savkare <jayesh.savkare.work@gmail.com>
2023-05-03 14:43:10 +0530

    Added 14 Tuples.py

---
+++ 14 Tuples.py
@@ -0,0 +1,10 @@
+# Tuples are kind of like Lists but Are Immutable ~ ` We cannot change them once made`
+
+numbers = (1,2,3)
+# Only this Methods work.
+# numbers.count()
+# numbers.index()
+
+# If We don't want to change any element then we use Tuple Instead of List.
+
+# DONE !
