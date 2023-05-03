bef37800294aeb3c0daef3f7e5db2eeceaa35cb9
Jayesh Savkare <jayesh.savkare.work@gmail.com>
2023-05-03 13:59:14 +0530

    Updated Lists.py

--- 10 Lists.py
+++ 10 Lists.py
@@ -1,14 +1,28 @@
 #Represent a list of Objects or List of Names.
 
 
-name = ["John", "Jayesh", "Jerry"]
+name = ["John", "Jayesh", "Jerry", "James"]
 
 print(name)
+# Output = ['John', 'Jayesh', 'Jerry', 'James']
+
+
 # If we have to print only John we will write number corresponding to it.
-#print(name[0])
+print(name[0])
 # Output = John
 
 
 # Python supports negative index.
-#print(name[0])
-# Output = Jerry
+print(name[-1])
+# Output = James
+
+
+# If we have to replace John with Jon ; We type number corresponding to it and then equals new value.
+# name [0]= "Jon"
+# print(name)
+# Output = ['Jon', 'Jayesh', 'Jerry', 'James']
+
+
+# We can also select a range of value. Here we are printing first three Words in List.
+print(name[0:3])
+# Output = ['John', 'Jayesh', 'Jerry']
