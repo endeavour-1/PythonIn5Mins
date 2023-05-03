543aa7e43ec9cc862feeb0fa5d7692cbceafc9aa
Jayesh Savkare <jayesh.savkare.work@gmail.com>
2023-05-03 14:26:12 +0530

    Added 12 For Loops.py

---
+++ 12 For Loops.py
@@ -0,0 +1,27 @@
+numbers = [1,2,3,4,5]
+
+print(numbers)
+# Output = [1, 2, 3, 4, 5]
+
+
+# If we want to print each value at new line then, we use For Loops.\
+# We used New item variable.
+for item in numbers:
+    print(item)
+# Output =
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+# We can also use While Loop But Code will be longer.
+i = 0 
+while i < len(numbers):
+    print(numbers[i])
+    i = i+1
+# Output is same.
+
+
+
